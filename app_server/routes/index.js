9e3c55ec55ed3c7653fb6ae62c11a00c85cf0e15
Daniel Jameson <danieljffs@gmail.com>
2023-10-25 19:42:14 +0100

    updated Index.js to annoy git

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
 
+// Adding a comment to annoy git
 /* Locations pages */
 router.get('/register', ctrlLocations.registerPage);
 router.get('/gamepage', ctrlLocations.gamePage);
